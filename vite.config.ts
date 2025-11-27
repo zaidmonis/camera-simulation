c71f35bc8e2876dee88b31ed6c2390046b161543
zaidmonis <mohammad.zaid@dominos.com.au>
2025-11-28 00:26:18 +0800

    add the repo name in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')?.[1];
+
 export default defineConfig({
+  base: repoName ? `/${repoName}/` : '/',
   plugins: [react()],
   server: {
     host: true
